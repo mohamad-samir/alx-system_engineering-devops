e043a38d11f62d9e3653eeebe2556583a826264f
mohamad-samir <mohamad.samir2011@gmail.com>
2023-08-01 20:25:55 +0300

    u

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+# This script accepts one argument and pass it to a regular expression matching method
+
+# Import the OptionParser class
+require 'optparse'
+
+# Initialize a new instance of OptionParser
+options = OptionParser.new do |opts|
+  # Define the option
+  opts.on("-i", "--input INPUT", "Input string") do |input|
+    # Perform the regular expression match
+    matches = input.scan(/School/)
+    # Print the matches
+    puts matches.join
+  end
+end
+
+# Parse the command-line options
+options.parse!
