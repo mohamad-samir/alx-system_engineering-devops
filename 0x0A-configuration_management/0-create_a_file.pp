e64121d3a11ded68b57df69abaca5a0022343d6e
mohamad-samir <mohamad.samir2011@gmail.com>
2023-09-22 20:38:06 +0300

    u

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-#creates a file
-
-file { 'create a file':
-  path    => '/tmp/school',
+file { '/tmp/school':
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
